7a2d14e962f5350e8e241a088e7073a3598813d4
Faizan Mustafa <faizanmustafa324@gmail.com>
2019-06-28 21:38:02 +0500

    Final Commit(Project Submitted)

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from shirts.models import Shirt, Creator
-from carts.models import Cart
+from carts.models import Cart, Item
 from shirts.models import Shirt, Background
 
 # Create your views here.
@@ -9,20 +9,18 @@ def index(request):
     shirts = Shirt.objects.order_by('-created_on')[:4]
     background = Background.objects.all().first()
     if request.user.is_authenticated and 'id' in request.session:
-        print("outer")
         shirt = Shirt.objects.all().filter(sess_id=request.session['id']).first()
         if shirt and request.session['id'] == shirt.sess_id:
-            print("inner")
-            creator = Creator.objects.all().filter(user=request.user).first()
-            if creator:
-                cart = creator.cart
-            else:
+            cart = Cart.objects.all().filter(user=request.user).first()
+            if not cart:
                 cart = Cart.objects.create(
                     user = request.user,
-                    total_price = 1500,
                 )
             Creator.objects.create(
+                shirt = shirt,
                 user = request.user,
+            )
+            Item.objects.create(
                 shirt = shirt,
                 cart = cart,
             )
